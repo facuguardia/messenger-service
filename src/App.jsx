1f0012baac37af92c49b7dd404fe6a02087b7c32
Facundo Guardia <contacto@facuguardia.com>
2022-10-20 22:58:37 -0300

    Cuarto commit (chat)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+// icons
 import {
   RiHome3Line,
   RiMoneyDollarCircleLine,
@@ -22,17 +23,17 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen ">
-      {/* // sidebar */}
+    <div className="min-h-screen">
+      {/* Sidebar */}
       <div
         className={`bg-white fixed w-6/8 md:w-96 h-full border-r p-8 flex flex-col justify-between overflow transition-all duration-700 lg:left-0 z-50 ${
           menu ? "left-0" : "-left-full"
         }`}
       >
         <div>
-          {/* logo */}
+          {/* Logo */}
           <h1 className="text-2xl uppercase font-bold mb-10">Tu Logo</h1>
-          {/* info usuario */}
+          {/* Info Usuario */}
           <div className="flex items-center gap-4 mb-10">
             <span className="bg-blue-100 py-2 px-[11px] rounded-full text-blue-700 font-medium">
               FG
@@ -119,7 +120,7 @@ function App() {
             </li>
           </ul>
         </div>
-        {/* logout */}
+        {/* Logout */}
         <ul>
           <li>
             <a
@@ -132,7 +133,7 @@ function App() {
           </li>
         </ul>
       </div>
-      {/* button menu */}
+      {/* Button Menu Movil */}
       <button
         onClick={toggleMenu}
         className="lg:hidden fixed bottom-6 right-6 border p-3 rounded-full bg-white "
@@ -151,16 +152,102 @@ function App() {
         <div className="bg-gray-100 grid grid-cols-1 md:grid-cols-4 p-4">
           <form className="col-span-1 p-4 border-r ">
             <div className="relative">
-            <RiSearchLine className="text-blue-900 absolute top-1 translate-y-1/2  left-2"/>
-              <input type="text" className="bg-white w-full py-2 px-10 outline-none rounded-lg border border-transparent focus:border-blue-700" placeholder="Search" />
+              <RiSearchLine className="text-blue-900 absolute top-1 translate-y-1/2  left-2" />
+              <input
+                type="text"
+                className="bg-white w-full py-2 px-10 outline-none rounded-lg border border-transparent focus:border-blue-700"
+                placeholder="Search"
+              />
             </div>
           </form>
           <div className="col-span-3 p-4 flex items-center justify-between">
             <h3 className="">Error code CR045</h3>
-            <p className="flex items-center gap-2 text-blue-900 "> <span className="text-gray-500">Transaction</span>123456{" "}<AiOutlineCopy className="hover:cursor-pointer text-xl"/></p>
+            <p className="flex items-center gap-2 text-blue-900 ">
+              {" "}
+              <span className="text-gray-500">Transaction</span>123456{" "}
+              <AiOutlineCopy className="hover:cursor-pointer text-xl" />
+            </p>
           </div>
         </div>
       </header>
+      {/* Main */}
+      <main className="lg:ml-96 pt-60 min-h-screen lg:w-[calc(100%-384px)] grid grid-cols-1 lg:grid-cols-4">
+        <ul className="col-span-1 border-r p-4">
+          <li>
+            <a href="#" className="hover:bg-gray-100 block rounded-lg p-2">
+              <h5 className="font-bold flex items-center gap-4">
+                <RiCheckboxBlankCircleFill className="text-[10px] text-orange-300" />
+                Lorem Ipsum
+              </h5>
+              <p className="text-gray-500 text-sm mb-2 mt-2">
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Placeat, voluptate!
+              </p>
+              <span className="text-xs text-gray-400 ">
+                Yesterday, 14:35 hrs
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:bg-gray-100 block rounded-lg p-2">
+              <h5 className="font-bold flex items-center gap-4">Lorem Ipsum</h5>
+              <p className="text-gray-500 text-sm mb-2 mt-2">
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Placeat, voluptate!
+              </p>
+              <span className="text-xs text-gray-400 ">
+                Yesterday, 14:35 hrs
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:bg-gray-100 block rounded-lg p-2">
+              <h5 className="font-bold flex items-center gap-4">Lorem Ipsum</h5>
+              <p className="text-gray-500 text-sm mb-2 mt-2">
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Placeat, voluptate!
+              </p>
+              <span className="text-xs text-gray-400 ">
+                Yesterday, 14:35 hrs
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:bg-gray-100 block rounded-lg p-2">
+              <h5 className="font-bold flex items-center gap-4">Lorem Ipsum</h5>
+              <p className="text-gray-500 text-sm mb-2 mt-2">
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Placeat, voluptate!
+              </p>
+              <span className="text-xs text-gray-400 ">
+                Yesterday, 14:35 hrs
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:bg-gray-100 block rounded-lg p-2">
+              <h5 className="font-bold flex items-center gap-4">Lorem Ipsum</h5>
+              <p className="text-gray-500 text-sm mb-2 mt-2">
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Placeat, voluptate!
+              </p>
+              <span className="text-xs text-gray-400 ">
+                Yesterday, 14:35 hrs
+              </span>
+            </a>
+          </li>
+        </ul>
+        <div className="col-span-3">
+          <div className="flex justify-center">
+            <p className="text-center border py-2 px-4 my-6 rounded-full">Yesterday</p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
